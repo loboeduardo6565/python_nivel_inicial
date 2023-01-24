c99301893f5399284eebebb0f396073260ae8c05
Eduardo Lobo <loboeduardo6565@gmail.com>
2023-01-23 21:26:58 -0300

    Introduzca un número e incremente su valor en 10%

---
+++ ejercicio5.py
@@ -0,0 +1,8 @@
+"""
+Escriba un programa que solicite un valor entero por pantalla y presente en la terminal
+editor el valor incrementado en un 10%.
+"""
+
+diez = int(input('Introduzca un número para ser incrementado en 10%: '))
+porcentaje = 10 * diez / 100
+print(porcentaje+diez)
