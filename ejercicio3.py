4dda3349001a52e09a1bc8d799ebc8c777301bf0
Eduardo Lobo <loboeduardo6565@gmail.com>
2023-01-23 21:25:47 -0300

    Calcular perímetro a partir de radio

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,6 +1,6 @@
 """
 Escriba un programa que solicite el radio de una circunferencia y permita calcular 
-el perímetro. Presente el resultado en la terminal del editor. 
+el perímetro de una circunferencia. Presente el resultado en la terminal del editor. 
 Utilice la siguiente fórmula:
 L = 2 · π · r
 L = Longitud de perímetro
