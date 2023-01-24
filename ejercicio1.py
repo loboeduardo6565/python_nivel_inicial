9c95198a17cb1454f31b6070b3a428b75ac74f76
Eduardo Lobo <loboeduardo6565@gmail.com>
2023-01-23 21:24:02 -0300

    tome tres valores por consola multiplique el primero por el segundo

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+"""
+Cree un programa que tome tres valores por consola multiplique el primero por el segundo 
+y le sume el tercero. Presente el resultado en la terminal del editor
+"""
+
+x = int(input('Introduzca un valor: '))
+y = int(input('Introduzca un valor: '))
+z = int(input('Introduzca un valor: '))
+
+resultado = ( x * y ) + z
+
+print(f'La multiplicación del primer numero por el segundo mas el tercer número es {resultado}')
+
