b6f7f8bec890265aa8a6d31ada78af92d69a5c41
Eduardo Lobo <loboeduardo6565@gmail.com>
2023-01-28 14:15:32 -0300

    Lista de objetos que imprimen una oración

---
+++ ejercicio2u2.py
@@ -0,0 +1,8 @@
+""" Cree una lista de frutas de 2 elementos, y realice un programa que muestre una oración
+    conteniendo los dos elementos de la lista concatenándolos con texto para formar una
+    oración con sentido. Presente el resultado en la terminal del editor.
+"""
+
+frutas = ["bananas", "manzanas"]
+
+print(f"Me gusta comer {frutas[1]} y {frutas[0]}")
