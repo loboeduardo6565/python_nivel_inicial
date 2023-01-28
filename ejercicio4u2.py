4bc74d1dcf17b02ae48d923c27f7299eeb4abce0
Eduardo Lobo <loboeduardo6565@gmail.com>
2023-01-28 14:17:18 -0300

    Ejemplo evaluando edad de jubilación y extructura if/else

---
+++ ejercicio4u2.py
@@ -0,0 +1,14 @@
+""" Realice un programa que consulte la edad y en caso de que el valor ingresado supere la
+    fecha de jubilación, presente en la terminal el mensaje “Ya está en edad de jubilarse" en 
+    caso contrario que presente Aún está en edad de trabajar”
+
+"""
+EDAD_JU=65 # Cambie la fecha de jubilación según desee
+EDAD_JU = int(EDAD_JU)
+edad = int(input("Introduzca su edad: "))
+
+if edad >= EDAD_JU:
+    print("Ya está en edad de jubilarse")
+else:
+    print("Aún está en edad de trabajar")
+
