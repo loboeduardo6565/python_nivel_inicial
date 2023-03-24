8b4cc354e5cef1455406ce4e9547fe3abb3a153a
Eduardo Lobo <loboeduardo6565@gmail.com>
2023-03-24 12:07:23 -0300

    Se agregan comentarios

--- ejercicio3u2.py
+++ ejercicio3u2.py
@@ -3,8 +3,10 @@ Tome dos valores por consola, y guarde en una lista: [primer_valor, segundo_valo
 
 """
 
-primer_valor = int(input("Introduzca un primer valor: "))
-segundo_valor = int(input("Introduzca un segundo valor: "))
+primer_valor = int(input("Introduzca un primer valor: ")) # Se asigna el primer valor ingresado por el usuario a la variable "primer_valor" usando la función "int()" para convertir el valor a un entero.
+segundo_valor = int(input("Introduzca un segundo valor: ")) # Se asigna el segundo valor ingresado por el usuario a la variable "segundo_valor" de la misma manera que el primer valor.
 
-suma = [primer_valor, segundo_valor, primer_valor + segundo_valor]
-print(f'La suma del primer valor más el segundo es: {suma[2]}')
+suma = [primer_valor, segundo_valor, primer_valor + segundo_valor] # Se crea una lista "suma" que contiene los valores de "primer_valor", "segundo_valor" y la suma de ambos.
+print(f'La suma del primer valor más el segundo es: {suma[2]}') # Finalmente, se imprime la suma de los dos valores en la consola usando la función "print()" y el formato "f" para incluir el valor de "suma[2]" en la cadena de texto.
+
+
