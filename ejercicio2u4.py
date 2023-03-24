ce50f4818bc77b23418f7dce6e563c76d7378212
Eduardo Lobo <loboeduardo6565@gmail.com>
2023-03-24 11:43:05 -0300

    Ejemplo de función LAMBDA, multiplicación

---
+++ ejercicio2u4.py
@@ -0,0 +1,11 @@
+"""Crear una función lambda que sea equivalente a la siguiente función:
+
+ def multiplicar_por_tres(valor):
+        res = 3 * valor
+        return res
+
+"""
+
+
+xtres = lambda valor: valor *3 
+print(f"{xtres(3)}")
