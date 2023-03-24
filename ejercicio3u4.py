2a1f5c01a7bf019f7e3bcbea11f197b169cbc24c
Eduardo Lobo <loboeduardo6565@gmail.com>
2023-03-24 11:43:36 -0300

    Ejemplo de función LAMBDA, suma de números

---
+++ ejercicio3u4.py
@@ -0,0 +1,12 @@
+
+""" 
+ Crear una función lambda que sea equivalente a la siguiente función:
+
+def sumar(valor1, valor2):
+    res = valor1 + valor2
+    return res
+
+"""
+
+suma = lambda a, b : a + b
+print(suma(2,5))
