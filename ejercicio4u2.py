01eed457db53ce16265bf9b5d186f583effdd777
Eduardo Lobo <loboeduardo6565@gmail.com>
2023-03-24 12:06:28 -0300

    Se agrega una condición if adicional

--- ejercicio4u2.py
+++ ejercicio4u2.py
@@ -7,7 +7,9 @@ EDAD_JU=65 # Cambie la fecha de jubilación según desee
 EDAD_JU = int(EDAD_JU)
 edad = int(input("Introduzca su edad: "))
 
-if edad >= EDAD_JU:
+if edad < 18:
+    print("Usted no tiene edad para trabajar")
+elif edad > 18 and edad >= EDAD_JU:
     print("Ya está en edad de jubilarse")
 else:
     print("Aún está en edad de trabajar")
